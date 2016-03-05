ab5fdb1dd0ac1575c1cff93d90d70093550919f3
MiniDi <naraevadiana@gmail.com>
2016-03-05 03:01:57 +0300

    Created Activity.

---
+++ app/src/main/java/com/example/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Диана on 05.03.2016.
+ */
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
